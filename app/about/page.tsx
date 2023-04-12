5bb5454229f7685cacee64e54b6efa5ebd6f7710
Carlos Bido <carloshbido@gmail.com>
2023-04-11 23:24:46 -0300

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, omnis. Atque officia totam velit tempore magni
+        voluptate aut, sed maxime molestiae ullam quam aperiam laborum saepe tempora labore explicabo consectetur?
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ app/components/Header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>Logo</div>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/about/team">Our Team</Link>
+          <Link href="/code/repos">Code</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
